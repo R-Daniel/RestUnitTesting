d65ec6ebc2e323bef1f938aefc4ea6c8c3cf228a
Sergio721 <oigres721@gmail.com>
2019-11-05 12:59:36 -0500

    persona

--- src/resources/personAPI.ts
+++ src/resources/personAPI.ts
@@ -2,8 +2,14 @@ import { REST_API } from "./meta/rest";
 
 export interface Person {
     name: string
+    age: number
+    mensaje: string
+
 }
 
 export default class PersonAPI extends REST_API<Person> {
 
+    saludar(){
+        
+    }
 }
