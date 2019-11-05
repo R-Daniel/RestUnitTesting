98a24250075c2f86858274b3a38c0e6bc96804f3
Daniel R. Vasquez Montes <dev.DanielR@gmail.com>
2019-11-05 13:52:12 -0500

    Added test function

--- src/resources/animalAPI.ts
+++ src/resources/animalAPI.ts
@@ -8,4 +8,12 @@ export interface Animal {
     real: boolean;
 }
 
-export class AnimalAPI extends REST_API<Animal> {}
+export class AnimalAPI extends REST_API<Animal> {
+    describe(n: number){
+        const animal:Animal = this.r(n);
+        if (animal.legs>0) console.log(`${animal.name} walks on ${animal.legs} legs!`);
+        if (animal.wings>0) console.log(`${animal.name} flies with ${animal.legs} wings!`);
+        if (animal.real) console.log(`${animal.name} is real!`);
+    }
+
+}
