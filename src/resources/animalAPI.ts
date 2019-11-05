0ccd06c507e2005ce14a297bb80367b7e4c8a722
Your Name <you@example.com>
2019-11-05 13:36:47 -0500

    Funcion Saludar

--- src/resources/animalAPI.ts
+++ src/resources/animalAPI.ts
@@ -2,8 +2,23 @@ import { REST_API } from "./meta/rest";
 
 export interface Animal {
     name: string
+    patas: number
+    genero: string
 }
 
 export default class AnimalAPI extends REST_API<Animal> {
+    mensaje: string
+
+    saludar(){
+        console.log(`Este es tu mensaje ${this.mensaje}`) 
+    }
+}
+
+let main = () => 
+{
+    let persona= new AnimalAPI()
+    persona.mensaje="Hola"
+    persona.saludar()
 
 }
+main()
